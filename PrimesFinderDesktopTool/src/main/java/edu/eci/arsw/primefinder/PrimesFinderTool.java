883f1b4350a61b1bd6af58d8e29737891cd2940a
diegofch29 <diegofch29@hotmail.com>
2020-09-10 13:07:12 -0500

    avance

--- PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimesFinderTool.java
+++ PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimesFinderTool.java
@@ -1,18 +1,15 @@
 package edu.eci.arsw.primefinder;
 
+
 import edu.eci.arsw.mouseutils.MouseMovementMonitor;
-import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
 
-public class PrimesFinderTool {
+
+
+public class PrimesFinderTool extends Thread{
 
 	public static void main(String[] args) {
 		            
@@ -20,11 +17,32 @@ public class PrimesFinderTool {
             
             PrimesResultSet prs=new PrimesResultSet("john");
             
-            PrimeFinder.findPrimes(new BigInteger("1"), new BigInteger("10000"), prs);
             
-            System.out.println("Prime numbers found:");
+            //PrimeFinder.findPrimes(new BigInteger("1"), new BigInteger("10000"), prs);
+            ArrayList<PrimeFinder> Finders =  new ArrayList<PrimeFinder>();
             
-            System.out.println(prs.getPrimes());
+            BigInteger start = new BigInteger("1");
+            BigInteger finish = new BigInteger("1000");
+            BigInteger lip = finish.subtract(start).divide(new BigInteger("4")).add(new BigInteger("1"));
+            BigInteger range = lip;
+            
+                    
+            for (int i = 0;i<4;i++){
+                System.out.println(i+"\t"+start+"\t"+range+"\t");
+                Finders.add(new PrimeFinder(start,range,prs));
+                start=range.add(new BigInteger("1"));
+                range=range.add(lip);
+            }
+            
+            Finders.forEach((i) -> {
+                i.start();
+                try {
+                    i.join();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(PrimesFinderTool.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+            boolean task_not_finished=true;
             
             
             /*while(task_not_finished){
@@ -42,6 +60,11 @@ public class PrimesFinderTool {
                     Logger.getLogger(PrimesFinderTool.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }*/
+            
+            System.out.println("Prime numbers found:");
+            
+            System.out.println(prs.getPrimes());
+
                         
             
             
